c0fe8f678f296e2f313a8c72f2fb95c913670406
aiolah <yoyo31.music@gmail.com>
2022-02-26 16:33:49 +0100

    Changement accès twig

--- moteur_template.php
+++ moteur_template.php
@@ -1,6 +1,6 @@
 <?php
 // Gestion des templates avec Twig
-	require_once('twig/vendor/autoload.php');
+	require_once('../twig/vendor/autoload.php');
   $loader = new \Twig\Loader\FilesystemLoader('Views');
   $twig = new \Twig\Environment($loader, [
   'cache' => false
